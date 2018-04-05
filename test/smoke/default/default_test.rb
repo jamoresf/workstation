836944ada9861dd957b89ee5594c7fa2d3ab281a
juanito <juanaito@calamidad.es>
2018-04-05 15:50:20 +0000

    Add additional tests for default test suite

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -21,4 +21,10 @@ describe package('tree') do
 it { should be_installed }
 end
 
+describe file('/etc/motd') do
+its('content') { should match(/Property of/) }
+it { should be_owned_by 'root' }
+end
+
+
 #end
